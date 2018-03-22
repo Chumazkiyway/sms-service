7c825d0ce3e47a28e2e98091d9cd655518d1e29f
Bogdan <bogdanchumak97@gmail.com>
2018-03-22 22:17:07 +0200

    Changed send.html and added dynamical table

---
+++ js/dyntable.js
@@ -0,0 +1,45 @@
+var t =  document.getElementById("dyntab");
+var tds = t.getElementsByTagName("td");
+for (var i=0; i<tds.length; i++){
+
+    tds[i].addEventListener('click',editRow);
+}
+
+
+function editRow(e) {
+      //ловим элемент, по которому кликнули
+      let t = e.target || e.srcElement;
+      //получаем название тега
+      let elm_name = t.tagName.toLowerCase();
+      //если это инпут - ничего не делаем
+      if(elm_name == 'input') {return false;}
+      let val = $(this).html();
+      let code = '<input type="text" id="edit" value="'+val+'" />';
+      $(this).empty().append(code);
+      $('#edit').focus();
+      $('#edit').blur(function()  {
+        let val = $(this).val();
+        $(this).parent().empty().html(val);
+      });
+  }
+
+
+$(window).keydown(function(event){
+  //ловим событие нажатия клавиши
+  if(event.keyCode == 13) { //если это Enter
+    $('#edit').blur();  //снимаем фокус с поля ввода
+  }
+});
+
+function addRow()
+{
+
+    document.getElementById("dyntab").insertRow(-1).innerHTML = '<td>QQ</td><td>qq</td><td>Qq</td>';
+
+    t =  document.getElementById("dyntab");
+    tds = t.getElementsByTagName("td");
+    for (var i=0; i<tds.length; i++){
+
+        tds[i].addEventListener('click',editRow);
+    }
+}
