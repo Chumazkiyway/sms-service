6424b2f95e7c16f0ed5f262ce608cb01c18f7225
Bogdan <bogdanchumak97@gmail.com>
2018-05-15 17:36:01 +0300

    add smsGateway

---
+++ react-backend/modules/sendToSmsGateway.js
@@ -0,0 +1,40 @@
+
+module.exports.sendMsg = function (xmlBody){
+  
+  var http = require("http");
+  var postRequest = {
+  	host: "gate.smsclub.mobi",
+    path: "/xml/",
+    port: 80,
+    method: "POST",
+    headers: {
+    	'Accept': 'text/xml',
+      'Content-Type': 'text/xml',
+    }
+  };
+
+  var buffer = "";
+
+  var req = http.request( postRequest, function( res ){
+
+     console.log( 'STATUS:' + res.statusCode);
+     var buffer = "";
+
+     res.on( "data", function( data ) {
+      buffer = buffer + data; 
+     } );
+
+     res.on( "end", function( data ) {
+      console.log( 'ResponseData:'+ buffer );
+     } );
+
+  });
+
+  req.on('error', function(e) {
+      console.log('problem with request: ' + e.message);
+  });
+
+  req.write(xmlBody);
+
+  req.end();
+}
--- react-backend/routes/accept.js
+++ react-backend/routes/accept.js
@@ -1,18 +1,37 @@
 var express = require('express');
 var router = express.Router();
-
+var req = require('modules/sendToSmsGateway')
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-	// Comment out this line:
-  //res.send('respond with a resource');
+  
 
-  // And insert something like this instead:
-  res.json([
-    ['Alexandr', 'Volik','0930685396','SMS'],
-    ['Alexandr', 'Volik','0930685396','SMS'],
-    ['Alexandr', 'Volik','0930685396','SMS'],
-    ['Alexandr', 'Volik','0930685396','SMS']
-  ]);
+
+var login = "380953346403"; //req.body.login
+var password = "9o8w2ts"; //req.body.password
+var alphaName = "club_bulk"; //req.body.alphaName
+var abonent ="380631001769;380930285396";
+var text = "Добрый час"; //req.body.text
+
+
+
+var xmlBody = "<?xml version='1.0' encoding='utf-8'?>" +
+              "<request_sendsms>" +
+                    "<username><![CDATA["+ login +"]]></username>" +
+                    "<password><![CDATA["+ password +"]]></password>" +
+                    "<from><![CDATA[" + alphaName +"]]></from>" +
+                    "<to><![CDATA[" + abonent + "]]></to>" +
+                    "<text><![CDATA[" + text +"]]></text>" +
+              "</request_sendsms>";
+
+req.sendMsg(xmlBody);
+
+
+  // res.json([
+  //   ['Alexandr', 'Volik','0930685396','SMS'],
+  //   ['Alexandr', 'Volik','0930685396','SMS'],
+  //   ['Alexandr', 'Volik','0930685396','SMS'],
+  //   ['Alexandr', 'Volik','0930685396','SMS']
+  // ]);
   
 });
 
